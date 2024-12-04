7b976ef37284a97f121a6fbb9f08283053e3cf46
OrionVi1998 <15966899+OrionVi1998@users.noreply.github.com>
2024-12-04 14:56:25 +0100

    Extract the onMouseDownAction of element into their own functions

--- Assets/Scripts/Element.cs
+++ Assets/Scripts/Element.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -8,6 +10,8 @@ namespace XR_Education_Project {
     {
         private GameManager gameManager;
         private UIManager uiManager;
+        private String action = null;
+        
         [HideInInspector] public ElementData elementData;
 
         void Start()
@@ -18,14 +22,45 @@ namespace XR_Education_Project {
 
         void OnMouseDown() // Replace with VR interaction later
         {
+            switch (action)
+            {
+                case "MainMenu":
+                    this.OnMainMenu();
+                    break;
+
+                case ("Instantiate"):
+                    this.InstantiateElement();
+                    break;
+
+                case (null): 
+                    break;
+            }
+            
+        }
+
+        private void OnMainMenu()
+        {
             if (gameManager.gameState == "menu" && elementData != null)
             {
                 uiManager.DisplayElementInfoPanel(elementData);
             }
-            else if (elementData == null )
+            else if (elementData == null)
             {
                 Debug.LogError("Element not found.");
             }
         }
+
+        private void InstantiateElement() {
+            Vector3 self_pos = gameObject.transform.position;
+            Vector3 pos = new Vector3(self_pos.x, self_pos.y, self_pos.z - 1);
+
+            var newElement = Instantiate(gameObject, pos, Quaternion.identity); // TODO: Change to a base prefab to fill with data
+        }
+
+        public void SetAction(String action)
+        {
+            this.action = action;
+        }
+
     }
 }
